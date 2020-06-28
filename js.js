a0b780a03e83775d107f1147d062abeaae244a45
Abel <abellegesse151@gmail.com>
2020-06-28 11:53:07 +0300

    Updated

--- js.js
+++ js.js
@@ -1,25 +1,21 @@
-document.querySelector('button').addEventListener('click',function(e){
-    var p=document.querySelector('#prin').value;
-    var y=document.querySelector('#year').value;
-    var rate=document.querySelector("#rate").value;
-    const num=1;
-    var answer=p*(num+(rate*y));
-    if(p.length==0&&y.length==0&&rate.length==0)
-    {
+document.querySelector('button').addEventListener('click', function(e) {
+    var principal = document.querySelector('#prin').value;
+    var year = document.querySelector('#year').value;
+    var rate = document.querySelector("#rate").value;
+    const num = 1;
+    var answer = principal * (num + (rate * year));
+    if (principal.length == 0 && year.length == 0 && rate.length == 0) {
         alert('Principal, Year and Rate can\'t be empty');
         e.preventDefault();
-    }
-    else if(p.length==0||y.length==0||rate.length==0)
-    {
+    } else if (principal.length == 0 || year.length == 0 || rate.length == 0) {
         alert('Please check if Principal, Year or Rate is empty');
         e.preventDefault();
-    }
-    else{
-        var boldText=document.createElement('b');
-        boldText.className='display-4 text-success change';
+    } else {
+        var boldText = document.createElement('b');
+        boldText.className = 'display-4 text-success change';
         boldText.appendChild(document.createTextNode(answer));
-        var parent=document.querySelector('.text-success');
-        var child=document.querySelector('.change');
+        var parent = document.querySelector('.text-success');
+        var child = document.querySelector('.change');
         parent.replaceWith(boldText);
         e.preventDefault();
     }
